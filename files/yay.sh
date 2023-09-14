36f5d6fad5440de1783253eb65a50ee8d63c515f
Fred <rider48371@gmail.com>
2023-09-13 22:18:49 -0400

    adding i3lock screen

--- files/yay.sh
+++ files/yay.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-yay -S qtile-extras picom-git spotify rofi-bluetooth-git
+yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color
 systemctl --user start clipmenud
 systemctl --user enable clipmenud
