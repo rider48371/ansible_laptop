f21673ca6847dc4eb37afa9a9bebd5ed58c2f748
Frederic Rider <rider48371@gmail.com>
2025-01-27 21:58:39 -0500

    removed incorrect yay.sh

--- files/.local/bin/yay.sh
+++ files/.local/bin/yay.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-yay -S qtile-extras picom-git spotify rofi-bluetooth-git
+yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color plex-desktop notion-app-electron 1password nordvpn-bin visual-studio-code-bin spotify proton-ge-custom-bin primevideo netflix disneyplus google-chrome caprine &
+systemctl --user start clipmenud &
+systemctl --user enable clipmenud
