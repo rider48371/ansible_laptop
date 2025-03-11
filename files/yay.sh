22c0347ac830a206f2c1eef3f06fcc3df5a2f20b
Fred Rider <rider48371@gmail.com>
2025-03-11 02:12:08 -0400

    updating yay.sh for clarity

--- files/yay.sh
+++ files/yay.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color plex-desktop notion-app-electron 1password nordvpn-bin visual-studio-code-bin spotify proton-ge-custom-bin primevideo netflix disneyplus google-chrome caprine &
+yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color plex-desktop notion-app-electron 1password nordvpn-bin visual-studio-code-bin spotify proton-ge-custom-bin primevideo netflix disneyplus google-chrome messenger-nativefier &
 systemctl --user start clipmenud &
 systemctl --user enable clipmenud
